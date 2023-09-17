0e2e005d8be69b7bd3c65a5105023bb00290c3ef
Ouokki <ouokki98@gmail.com>
2023-09-17 19:34:34 +0200

    structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { CalendarComponent } from './pages/calendar/calendar.component';
+import { PaymentComponent } from './pages/payment/payment.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo:'/dashboard' , pathMatch: 'full'},
+  {path:'dashboard' , component:DashboardComponent},
+  {path:'profile' , component:ProfileComponent},
+  {path:'calendar' , component:CalendarComponent},
+  {path:'payment' , component:PaymentComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { CalendarComponent } from './pages/calendar/calendar.component';
+import { PaymentComponent } from './pages/payment/payment.component';
+import { MaterialModulesModule } from './shared/material-modules/material-modules.module';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    ProfileComponent,
+    CalendarComponent,
+    PaymentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModulesModule
+
+
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
